32aa94fb512b618cded989a40d6b6fd158bdad19
Kunto Aji. K <kunto@dropsuite.com>
2022-09-08 16:55:38 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQSSendMessages
+{
+  class Program
+  {
+    // Some example messages to send to the queue
+    private const string JsonMessage = "{\"product\":[{\"name\":\"Product A\",\"price\": \"32\"},{\"name\": \"Product B\",\"price\": \"27\"}]}";
+
+    static async Task Main(string[] args)
+    {
+
+      // Do some checks on the command-line
+      if(args.Length == 0)
+      {
+        Console.WriteLine("\nUsage: SQSSendMessages queue_url");
+        Console.WriteLine("   queue_url - The URL of an existing SQS queue.");
+        return;
+      }
+      if(!args[0].StartsWith("https://sqs."))
+      {
+        Console.WriteLine("\nThe command-line argument isn't a queue URL:");
+        Console.WriteLine($"{args[0]}");
+        return;
+      }
+
+      // Create the Amazon SQS client
+      var sqsClient = new AmazonSQSClient();
+
+      // (could verify that the queue exists)
+      // Send some example messages to the given queue
+      // A single message
+      await SendMessage(sqsClient, args[0], JsonMessage);
+    }
+
+
+    // Method to put a message on a queue
+    // Could be expanded to include message attributes, etc., in a SendMessageRequest
+    private static async Task SendMessage(
+      IAmazonSQS sqsClient, string qUrl, string messageBody)
+    {
+      SendMessageResponse responseSendMsg =
+        await sqsClient.SendMessageAsync(qUrl, messageBody);
+      Console.WriteLine($"Message added to queue\n  {qUrl}");
+      Console.WriteLine($"HttpStatusCode: {responseSendMsg.HttpStatusCode}");
+    }
+  }
+}
